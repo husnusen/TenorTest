ccf3e6fcded74793770296923bca4a4dc84e7615
husnusen <husnu.shen@gmail.com>
2018-06-19 01:31:27 +0000

    Rename methods

--- TenorExercise.UnitTest/FileReaderWriterUnitTest.cs
+++ TenorExercise.UnitTest/FileReaderWriterUnitTest.cs
@@ -26,15 +26,22 @@ namespace TenorExercise.UnitTest
             _tenorReaderWriter = new TenorReadWriter(_fileReader.Object, _fileWriter.Object);
         }
 
+
+        /// <summary>
+        /// Check that is the file exist or not
+        /// </summary>
         [Test]
-        void IsFileExist()
+        void CheckIsFileExist_Test()
         {
             Assert.IsTrue(_tenorReaderWriter.IsFileExist("test"));
 
         }
 
+        /// <summary>
+        /// Test for clos streams.
+        /// </summary>
         [Test]
-        void Close()
+        void CloseAllStreams_Test()
         {
             _tenorReaderWriter.Close();
 
@@ -42,54 +49,76 @@ namespace TenorExercise.UnitTest
             _fileWriter.Verify(y => y.Close(), Times.Once);
         }
 
+        /// <summary>
+        /// Test to open file to read
+        /// </summary>
         [Test]
-        void OpenToRead()
+        void OpenToReadFile_Test()
         {
             _tenorReaderWriter.OpenForRead("data.txt");
             _fileReader.Verify(x => x.Open("data.txt"), Times.Once);
 
         }
 
+        /// <summary>
+        /// Test to create new file to write new list
+        /// </summary>
         [Test]
-        void OpenToWrite()
+        void OpenFileToWrite_Test()
         {
             _tenorReaderWriter.OpenForWrite("3.txt");
             _fileWriter.Verify(x => x.Open("3.txt"), Times.Once);
 
         }
 
+        /// <summary>
+        /// Test to read file
+        /// </summary>
         [Test]
-        void Read()
+        void ReadListFromFile_Test()
         {
             var result = _tenorReaderWriter.Read();
             Assert.IsNotEmpty(result);
         }
 
-
+        /// <summary>
+        /// Test to write file
+        /// </summary>
         [Test]
-        void Write()
+        void WriteListToFile_Test()
         {
             _tenorReaderWriter.Write("test");
             _fileWriter.Verify(x => x.Write("test"), Times.Once);
 
         }
 
+
+        /// <summary>
+        /// Test to get tenor list from file as List
+        /// </summary>
         [Test]
-        void GetTenorList()
+        void GetTenorList_Test()
         {
             var result = _tenorReaderWriter.GetTenorList();
             Assert.IsNotNull(result);
         }
 
+
+        /// <summary>
+        /// Test to order by Portfolio and Tenor
+        /// </summary>
         [Test]
-        void OrderByPortfolioIdAndTenor()
+        void OrderByPortfolioIdAndTenor_Test()
         {
             var result = _tenorReaderWriter.OrderByPortfolioIdAndTenor(_tenorReaderWriter.GetTenorList());
             Assert.IsNotNull(result);
         }
 
+        /// <summary>
+        /// Test to order by Tenor and Portfolio
+        /// </summary>
         [Test]
-        void OrderByTenorAndPortfolioID()
+        void OrderByTenorAndPortfolioID_Test()
         {
             var result = _tenorReaderWriter.OrderByTenorAndPortfolioID(_tenorReaderWriter.GetTenorList());
             Assert.IsNotNull(result);
